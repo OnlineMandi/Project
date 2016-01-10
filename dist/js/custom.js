79ea230b2eccf138236c99d0ecee625ee0dfceb4
Gurpal <gurpal713@gmail.com>
2016-01-10 12:37:01 +0530

    home page slider done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -4,9 +4,18 @@ $(document).ready(function(){
 
        $("body").on("mouseover","text.headline,text.headline1",function(){
            $(this).css("fill","url('#mypattern1') none");
-       })
+       });
        $("body").on("mouseout","text.headline,text.headline1",function(){
            $(this).css("fill","url('#mypattern') none");
-       })
+       });
+
+
+        $('.bxslider').bxSlider({
+            auto: true,
+            autoControls: true,
+            nextText: '',
+            prevText: ''
+        });
+
 });
 
